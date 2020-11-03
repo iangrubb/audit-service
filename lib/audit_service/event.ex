db9fc4468937bfa6256b6c20f32309517dc24044
IanGrubb <hi@iangrubb.com>
2020-11-03 16:06:54 -0500

    Adds event changeset

--- lib/audit_service/event.ex
+++ lib/audit_service/event.ex
@@ -1,13 +1,30 @@
 defmodule AuditService.Event do
 
     use Ecto.Schema
+    import Ecto.Changeset
 
     schema "event" do
         field :action, :string
         field :initiator, :string
         field :resource, :string
         field :timestamp, :utc_datetime
-        field :details, :map
+        field :details, :map, default: %{}
+    end
+
+    def changeset(event, params \\ %{}) do
+        event
+        |> cast(params, [:action, :initiator, :resource, :timestamp, :details])
+        |> validate_required([:action, :initiator])
+        |> validate_change(:details, fn :details, details ->
+            details
+            |> Map.keys()
+            |> Enum.filter(fn key ->
+                key == "resource" || key == "initiator"
+            end)
+            |> Enum.map(fn key -> 
+                [details: "#{key} is not a valid key"]
+            end)
+        end)
     end
 
 end
